5c588cf6333d68604f94782415b30f7bf0589ca7
Rizqdwan <79435880+Rizqdwan@users.noreply.github.com>
2024-06-15 10:56:28 +0700

    Update authServices.js

--- src/config/authServices.js
+++ src/config/authServices.js
@@ -5,26 +5,30 @@ dotenv.config();
 const config = {
   port: process.env.PORT || 3000,
   host: process.env.HOST,
-  // googleServiceCreden: JSON.parse(process.env.CREDENTIALS),
+  googleServiceCreden: {
+    project_id: process.env.CREDENTIALS_PROJECT_ID,
+    client_email: process.env.CREDENTIALS_CLIENT_EMAIL,
+    private_key: process.env.CREDENTIALS_PRIVATE_KEY
+  },
   secret: process.env.SECRET_KEY,
   bucketName: process.env.STORAGE_NAME,
 };
 
-// const firestoreAuth = {
-//   projectId: config.googleServiceCreden.project_Id,
-//   credentials: {
-//     client_email: config.googleServiceCreden.client_email,
-//     private_key: config.googleServiceCreden.private_key
-//   }
-// }
+const firestoreAuth = {
+  projectId: config.googleServiceCreden.project_Id,
+  credentials: {
+    client_email: config.googleServiceCreden.client_email,
+    private_key: config.googleServiceCreden.private_key
+  }
+}
 
 
-// const storageAuth = {
-//   projectId: config.googleServiceCreden.project_Id,
-//   credentials: {
-//     client_email: config.googleServiceCreden.client_email,
-//     private_key: config.googleServiceCreden.private_key
-//   }
+const storageAuth = {
+  projectId: config.googleServiceCreden.project_I,
+  credentials: {
+    client_email: config.googleServiceCreden.client_email,
+    private_key: config.googleServiceCreden.private_key
+  }
 }
 
 module.exports = { config, firestoreAuth, storageAuth };
