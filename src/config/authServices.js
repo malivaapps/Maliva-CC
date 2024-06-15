316acbeabc3662e7a16ee4ce4baca026604c5e2b
Rizqdwan <79435880+Rizqdwan@users.noreply.github.com>
2024-06-15 10:36:23 +0700

    Update authServices.js

--- src/config/authServices.js
+++ src/config/authServices.js
@@ -5,26 +5,26 @@ dotenv.config();
 const config = {
   port: process.env.PORT || 3000,
   host: process.env.HOST,
-  googleServiceCreden: process.env.CREDENTIALS,
+  // googleServiceCreden: JSON.parse(process.env.CREDENTIALS),
   secret: process.env.SECRET_KEY,
   bucketName: process.env.STORAGE_NAME,
 };
 
-const firestoreAuth = {
-  projectId: config.googleServiceCreden.project_Id,
-  credentials: {
-    client_email: config.googleServiceCreden.client_email,
-    private_key: config.googleServiceCreden.private_key
-  }
-}
+// const firestoreAuth = {
+//   projectId: config.googleServiceCreden.project_Id,
+//   credentials: {
+//     client_email: config.googleServiceCreden.client_email,
+//     private_key: config.googleServiceCreden.private_key
+//   }
+// }
 
 
-const storageAuth = {
-  projectId: config.googleServiceCreden.project_Id,
-  credentials: {
-    client_email: config.googleServiceCreden.client_email,
-    private_key: config.googleServiceCreden.private_key
-  }
+// const storageAuth = {
+//   projectId: config.googleServiceCreden.project_Id,
+//   credentials: {
+//     client_email: config.googleServiceCreden.client_email,
+//     private_key: config.googleServiceCreden.private_key
+//   }
 }
 
 module.exports = { config, firestoreAuth, storageAuth };
